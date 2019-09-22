6c4f5b619e631426751d5976ca8ac3c247de45c4
Seafsummer <871188077@qq.com>
2019-09-22 22:14:06 +0800

    完成歌词滚动设置校准以及即时响应音频等问题；播放控制初步形成。

---
+++ LrcView/LrcEntry.java
@@ -0,0 +1,91 @@
+package com.cloud.music.LrcView;
+
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.text.format.DateUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 一行歌词实体
+ */
+class LrcEntry implements Comparable<LrcEntry> {
+    private long time;
+    private String text;
+    private StaticLayout staticLayout;
+    /**
+     * 歌词距离视图顶部的距离
+     */
+    private float offset = Float.MIN_VALUE;
+    public static final int GRAVITY_LEFT = 1;
+    public static final int GRAVITY_CENTER = 2;
+    public static final int GRAVITY_RIGHT = 3;
+
+    LrcEntry(long time, String text) {
+        this.time = time;
+        this.text = text;
+    }
+
+    void init(TextPaint paint, int width, int gravity) {
+        Layout.Alignment align;
+        switch (gravity) {
+            case GRAVITY_LEFT:
+                align = Layout.Alignment.ALIGN_NORMAL;
+                break;
+
+            default:
+            case GRAVITY_CENTER:
+                align = Layout.Alignment.ALIGN_CENTER;
+                break;
+
+            case GRAVITY_RIGHT:
+                align = Layout.Alignment.ALIGN_OPPOSITE;
+                break;
+        }
+        staticLayout = new StaticLayout(text, paint, width, align, 1f, 0f, false);
+
+        offset = Float.MIN_VALUE;
+    }
+
+    long getTime() {
+        return time;
+    }
+
+    StaticLayout getStaticLayout() {
+        return staticLayout;
+    }
+
+    int getHeight() {
+        if (staticLayout == null) {
+            return 0;
+        }
+        return staticLayout.getHeight();
+    }
+
+    public float getOffset() {
+        return offset;
+    }
+
+    public void setOffset(float offset) {
+        this.offset = offset;
+    }
+
+    @Override
+    public int compareTo(LrcEntry entry) {
+        if (entry == null) {
+            return -1;
+        }
+        return (int) (time - entry.getTime());
+    }
+}
